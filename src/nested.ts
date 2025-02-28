c798c20608bf3123f85a5334d748fde076920e7b
Gael Lucero <gael@udel.edu>
2025-02-28 00:52:58 -0500

    Completed 7th function

--- src/nested.ts
+++ src/nested.ts
@@ -62,7 +62,8 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    let sum = questions.reduce((sum: number, question: Question) => question.published ? sum + question.points : sum, 0)
+    return sum;
 }
 
 /***
