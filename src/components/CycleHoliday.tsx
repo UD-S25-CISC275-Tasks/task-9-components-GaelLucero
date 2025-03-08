ec232fa01f04a645749b8105f1832c86e8305828
Gael Lucero <gael@udel.edu>
2025-03-08 13:46:57 -0500

    Completed final two features

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,61 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    /*
+    Day of the Dead: 💀
+    Christmas: 🎄
+    New Years: 🎆
+    Chinese New Year: 🐲
+    Thanksgiving: 🦃
+
+    */
+
+    const [holiday, setHoliday] = useState<Holiday>("New_Years");
+    
+    type Holiday = "Day_of_the_Dead" | "Christmas" | "New_Years" | "Chinese_New_Years" | "Thanksgiving";
+
+    const Date_Map: Record<Holiday, Holiday> = {
+        "New_Years" : "Chinese_New_Years",
+        "Chinese_New_Years" : "Day_of_the_Dead",
+        "Day_of_the_Dead" : "Thanksgiving",
+        "Thanksgiving" : "Christmas",
+        "Christmas" : "New_Years"
+    };
+
+    const Alphabet__Map: Record<Holiday, Holiday> = {
+        "Chinese_New_Years" : "Christmas",
+        "Christmas" : "Day_of_the_Dead",
+        "Day_of_the_Dead" : "New_Years",
+        "New_Years" : "Thanksgiving",
+        "Thanksgiving" : "Chinese_New_Years"
+    };
+
+    function changeByAlphabet() {
+        let newColor = Alphabet__Map[holiday];
+        setHoliday(newColor);
+    }
+
+    function changeByDate(){
+        let newColor = Date_Map[holiday];
+        setHoliday(newColor);
+    }
+
+    function displayEmoji() {
+        if (holiday === "Chinese_New_Years") {
+            return "🐲";
+        }else if (holiday === "New_Years") {
+            return "🎆";
+        }else if (holiday === "Christmas") {
+            return "🎄";
+        }else if (holiday === "Day_of_the_Dead") {
+            return "💀";
+        }
+        return "🦃";
+    }
+
+    return <div>
+        <Button onClick={changeByAlphabet}>Advance by Alphabet</Button>
+        <Button onClick={changeByDate}>Advance by Year</Button>
+        Holiday: {displayEmoji()}
+    </div>;
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -6,11 +6,37 @@ import { Button } from "react-bootstrap";
  * The function uses the builtin `random` function of the `Math`
  * module (which returns a random decimal between 0 up until 1) in order
  * to produce a random integer between 1 and 6 (inclusive).
+ * 
  */
+
 export function d6(): number {
     return 1 + Math.floor(Math.random() * 6);
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [rightRoll, setRightRoll] = useState<number>(1);
+    const [leftRoll, setLeftRoll] = useState<number>(6);
+
+    function displayLeft(){
+        let roll = d6();
+        setLeftRoll(roll);
+    }
+
+    function displayRight(){
+        let roll = d6();
+        setRightRoll(roll);
+    }
+
+    return <div>
+        <span data-testid = "left-die">
+    <Button onClick={displayLeft}>Roll Left</Button>
+    Left Roll: {leftRoll}
+  </span>;
+
+  <span data-testid = "right-die">
+    <Button onClick={displayRight}>Roll Right</Button>
+    Right Roll: {rightRoll}
+  </span>;
+  {((leftRoll === rightRoll) && leftRoll !== 1 ? "Win" : (leftRoll === rightRoll) ? "Lose" : "")}
+    </div>;
 }
